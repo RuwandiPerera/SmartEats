156dd6c2280d35d87c9c730f4e91eb14c3d550f3
RuwandiPerera <139639701+RuwandiPerera@users.noreply.github.com>
2025-06-01 13:05:19 +0530

    Backend Folder Initialized

---
+++ Backend/src/test/java/com/smarteats/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarteats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
